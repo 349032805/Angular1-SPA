9633809f6fd6f4c46dd12530cb5c455f5b8bcf8c
349032805@qq.com <349032805@qq.com>
2016-11-20 18:17:01 +0800

    修改gruntfil生产环境的配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,4 @@
-// Generated on 2016-07-17 using generator-angular 0.15.1
 'use strict';
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
 
   // Time how long tasks take. Can help when optimizing build times
