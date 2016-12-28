fb969f8c667f7fb59bbcd6681bc45b30d7145020
349032805@qq.com <349032805@qq.com>
2016-12-28 09:41:45 +0800

    git commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,7 +12,6 @@ angular
   .module('fantasyadminApp', [
     'ngResource',
     'ui.router',
-    'ui.bootstrap',
     'ngMessages'
   ])
   .config(function ($stateProvider, $urlRouterProvider) {
--- app/scripts/config/http_interceptor.js
+++ app/scripts/config/http_interceptor.js
@@ -2,7 +2,8 @@
 
   angular.module('fantasyadminApp').factory('httpInterceptor', [ '$q', '$injector',function($q, $injector) { 
     var httpInterceptor = { 
-      'responseError' : function(response) { 
+      'responseError' : function(response) {
+        var tipMsg = "";
         if (response.status === 401) { 
           var rootScope = $injector.get('$rootScope'); 
           var state = $injector.get('$rootScope').$state.current.name; 
@@ -10,25 +11,23 @@
           rootScope.$state.go("login"); 
 
         } else if (response.status === 403) {
-          angular.element('#server_error_msg').text('您没有权限访问该网页...');
-          angular.element('#modal_server_error').modal('show');
+          tipMsg = "您没有权限访问该网页...";
 
         } else if (response.status === 404) {
-          angular.element('#server_error_msg').text('您访问的网页不存在...');
-          angular.element('#modal_server_error').modal('show');
+          tipMsg = "您访问的网页不存在...";
 
-        } else if (response.status === 400) {
-          angular.element('#server_error_msg').text('服务器出错，请稍后重试...');
-          angular.element('#modal_server_error').modal('show');
-
-        } else if (response.status === 500) {
-          angular.element('#server_error_msg').text('服务器出错，请稍后重试...');
-          angular.element('#modal_server_error').modal('show');
+        } else if (response.status === 400 || response.status === 500) {
+          tipMsg = "服务器出错，请稍后重试...";
 
         } else if (response.status === 409) {
-          angular.element('#server_error_msg').text(response.statusText);
-          angular.element('#modal_server_error').modal('show');
+          tipMsg = response.statusText;
         }
+
+        if(tipMsg !=""){
+           angular.element('#server_error_msg').text(tipMsg);
+           angular.element('#modal_server_error').modal('show');
+        }
+
          return $q.reject(response); 
       }, 
       'response' : function(response) { 
